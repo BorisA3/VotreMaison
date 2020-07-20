b77bc8a1908d1dd7e3177552865446ecc5e4228a
MarcoRodriguezM <57971630+MarcoRodriguezM@users.noreply.github.com>
2020-07-19 18:25:27 -0600

    Upload del proyecto

---
+++ MaisonVotre/Models/Prueba.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MaisonVotre.Models
+{
+    public class Prueba
+    {
+        [Key]
+        public int PruebaId { get; set; }
+
+        public string PruebaName { get; set; }
+
+    }
+}
---
+++ MaisonVotre/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MaisonVotre.Startup))]
+namespace MaisonVotre
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
