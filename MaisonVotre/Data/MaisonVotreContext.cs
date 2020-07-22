3ccd9c38476d114ebd124a41a440c6696e7f1349
MarcoRodriguezM <57971630+MarcoRodriguezM@users.noreply.github.com>
2020-07-22 16:02:31 -0600

    UpdateCorrection

--- MaisonVotre/Data/MaisonVotreContext.cs
+++ MaisonVotre/Data/MaisonVotreContext.cs
@@ -19,6 +19,19 @@ namespace MaisonVotre.Data
         {
         }
 
-        
+        public System.Data.Entity.DbSet<MaisonVotre.Models.Categoria> Categorias { get; set; }
+
+        public System.Data.Entity.DbSet<MaisonVotre.Models.Ciudad> Ciudads { get; set; }
+
+        public System.Data.Entity.DbSet<MaisonVotre.Models.Cliente> Clientes { get; set; }
+
+        public System.Data.Entity.DbSet<MaisonVotre.Models.Empresa> Empresas { get; set; }
+
+        public System.Data.Entity.DbSet<MaisonVotre.Models.Pedido> Pedidos { get; set; }
+
+        public System.Data.Entity.DbSet<MaisonVotre.Models.PedidoDetalle> PedidoDetalles { get; set; }
+
+        public System.Data.Entity.DbSet<MaisonVotre.Models.Producto> Productos { get; set; }
+
     }
 }
