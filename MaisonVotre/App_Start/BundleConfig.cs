d3102b64c1dba28c8e072994ff6717426ed82288
BorisA3 <botisalarcon@gmail.com>
2020-07-27 16:53:40 -0600

    Actualizacion

--- MaisonVotre/App_Start/BundleConfig.cs
+++ MaisonVotre/App_Start/BundleConfig.cs
@@ -14,29 +14,6 @@ namespace MaisonVotre
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/template").Include(
-                        "~/Scripts/jquery.dropotron.min.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template2").Include(
-                        "~/Scripts/breakpoint.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template3").Include(
-                        "~/Scripts/browser.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template4").Include(
-                        "~/Scripts/main.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template5").Include(
-                        "~/Scripts/util.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template6").Include(
-                        "~/Scripts/jquery.min.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template7").Include(
-                        "~/Scripts/jquery.scrollex.min.css"));
-
-            bundles.Add(new ScriptBundle("~/bundles/template8").Include(
-                        "~/Scripts/jquery.scrolly.min.css"));
 
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
@@ -50,8 +27,7 @@ namespace MaisonVotre
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/xcss").Include(
-                      "~/Content/TemplateCustom/styles.css"));
+          
         }
     }
 }
